b5aef6d6a20f7d56982f4177aafb50a5778bf32d
Jouda Hidri <hidrijouda@gmail.com>
2020-07-25 21:51:38 +0200

    implement retry

--- src/main/java/com/example/demo/service/Consumer.java
+++ src/main/java/com/example/demo/service/Consumer.java
@@ -4,13 +4,22 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
+	
+    private final Producer producer;
 
-    private final Logger logger = LoggerFactory.getLogger(Producer.class);
+    @Autowired
+    Consumer(Producer producer) {
+        this.producer = producer;
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
     @KafkaListener(topics = "broadway", groupId = "group_id")
     public void consume(String message) throws IOException {
@@ -20,5 +29,8 @@ public class Consumer {
     @KafkaListener(topics = "broadway_err", groupId = "group_id")
     public void consumeErr(String message) throws IOException {
         logger.info(String.format("#### -> received error -> %s", message));
+        // retry
+        String corrected = message.replaceAll("[^0-9.]", "");
+        this.producer.sendMessage(corrected);
     }
 }
